0a97440927fb62e4dd08c978d6388ceea797f8f0
Taz Hinkle <hinkle.taz@gmail.com>
2020-07-11 12:19:03 -0600

    added preliminary php files

---
+++ phpdemo.php
@@ -0,0 +1,13 @@
+<html>
+    <head><title>
+        Hello Forms
+    </title></head>
+    <body>
+        <form action="process.php" method="post">
+            Enter your name: <input name="name" type="test">
+            <input type="submit">
+        </form>
+        $dev = "Taz";
+        echo $dev . " says hello";
+    </body>
+</html>
---
+++ process.php
@@ -0,0 +1,11 @@
+<html>
+    <head><title>
+        Hello Forms
+    </title></head>
+    <body>
+        <?php
+            $name = $_POST["name"];
+            echo "Hello, " . name;
+        ?>
+    </body>
+</html>
