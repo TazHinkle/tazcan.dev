5dba2e23e575210b2f374d437d67045a6b6b2148
TazHinkle <hinkle.taz@gmail.com>
2023-05-31 13:39:54 -0600

    + openGraph tags & favicon

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,6 @@ export default defineConfig({
   server: {
     host: true,
   },
-  base: process.env.NODE_ENV === 'production' ? '/tazcan_dev/' : '/',
+  // base: process.env.NODE_ENV === 'production' ? '/tazcan_dev/' : '/',
   plugins: [vue()]
 })
